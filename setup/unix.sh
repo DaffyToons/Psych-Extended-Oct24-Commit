e8f3145e56f89c7f857d56560796e27f95f10d8d
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2026-07-23 13:49:12 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -17,10 +17,10 @@ haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet #That clearly my repo
 haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
-haxelib install lime 8.0.1 --quiet
-haxelib set lime 8.0.1
-haxelib install openfl 9.2.1 --quiet
-haxelib set openfl 9.2.1
+haxelib install lime 8.0.2 --quiet
+haxelib set lime 8.0.2
+haxelib install openfl 9.2.2 --quiet
+haxelib set openfl 9.2.2
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
 haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git 57f5d47f2533fd0c3dcd025a86cb86c0dfa0b6d2 --quiet
